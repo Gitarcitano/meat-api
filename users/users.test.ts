a8aa9d52f0747a02d2175821fee7fb69eb590578
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2020-02-16 21:31:40 -0300

    Update users.test.ts

--- users/users.test.ts
+++ users/users.test.ts
@@ -6,10 +6,12 @@ import { environment } from '../common/environment'
 import { usersRouter } from './users.router'
 import { User } from './users.model'
 
+let address: string
 let server: Server
 beforeAll(()=>{
   environment.db.url =  process.env.DB_URL || 'mongodb://localhost/meat-api-test-db'
   environment.server.port = process.env.SERVER_PORT || 3001
+  address = `http://localhost:${environment.server.port}`
   server = new Server()
   return server.bootstrap([usersRouter])
                .then(()=>User.deleteMany({}).exec())
@@ -17,7 +19,7 @@ beforeAll(()=>{
 })
 
 test('get /users', ()=>{
-  return request('http://localhost:3001')
+  return request(address)
     .get('/users')
     .then(response =>{
       expect(response.status).toBe(200)
@@ -26,7 +28,7 @@ test('get /users', ()=>{
 })
 
 test('post /users', ()=>{
-  return request('http://localhost:3001')
+  return request(address)
   .post('/users')
   .send({
     name: 'usuario1',
@@ -44,6 +46,37 @@ test('post /users', ()=>{
   }).catch(fail)
 })
 
-// test('delete /users', ()=>{
+test('get /users/aaaaa - not found', ()=>{
+  return request(address)
+  .get('/users/aaaaa')
+  .then(response =>{
+    expect(response.status).toBe(404)
+  }).catch(fail)
+})
 
-// })
+test('patch /users/:id', ()=>{
+  return request(address)
+  .post('/users')
+  .send({
+    name: 'usuario2',
+    email: 'usuario2@email.com',
+    password: '123456'
+  })
+  .then(response => request(address)
+                    .patch(`/users/${response.body._id}`)
+                    .send({
+                      name: 'usuario2 - patch'
+                    }))
+  .then(response=>{
+    expect(response.status).toBe(200)
+    expect(response.body._id).toBeDefined()
+    expect(response.body.name).toBe('usuario2 - patch')
+    expect(response.body.email).toBe('usuario2@email.com')
+    expect(response.body.password).toBeUndefined()
+  })                    
+  .catch(fail)
+})
+
+afterAll(()=>{
+  return server.shutdown()
+})
