848a56a9372af2a5b842d159f81a60fc681749bb
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2020-02-16 21:31:35 -0300

    server shutdown

--- server/server.ts
+++ server/server.ts
@@ -53,4 +53,8 @@ export class Server {
                this.initRoutes(routers).then(() => this))
     }
 
+    shutdown(){
+      return mongoose.disconnect().then(()=>this.application.close())
+    }
+
 }
